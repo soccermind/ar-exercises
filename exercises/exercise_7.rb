958725a671d41078ad4e297c0b710cc236659523
Alberto Olvera Sotres <mty2yyz@gmail.com>
2021-01-05 17:05:25 -0500

    Completed exercise_7, fixed exercise_6 to meet validations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,13 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter new store name: "
+store_name = gets.chomp
+store = Store.create(name: store_name)
+puts "----- Store validation errors -----"
+puts store.errors.full_messages
+
+employee = @store1.employees.create(hourly_rate: 20)
+puts "----- Employee validation errors -----"
+puts employee.errors.full_messages
+
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates_associated :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  validate :must_carry_at_least_one_type_or_apparel, on: :create
+
+  def must_carry_at_least_one_type_or_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "can't be false if womens_apparel is false")
+      errors.add(:womens_apparel, "can't be false if mens_apparel is false")
+    end
+  end
 end
